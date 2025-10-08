1139e31e6dc86e10966c62f6a02fc5dd371ca65f
slide <paulminesangulo@yahoo.com>
2025-10-08 15:13:34 +0800

    Add path variable in start route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { StartRouteComponent } from './maps/start-route/start-route.component';
 export const routes: Routes = [
   { path: 'maps/current-location', component: CurrentLocationComponent },
   { path: 'maps/route/:coordinates', component: RouteComponent },
-  { path: 'maps/start-route', component: StartRouteComponent },
+  { path: 'maps/start-route/:coordinates', component: StartRouteComponent },
   { path: 'maps/test', component: TestComponent },
   { path: '', redirectTo: 'maps/current-location', pathMatch: 'full' }
 ];
--- src/app/maps/start-route/start-route.component.ts
+++ src/app/maps/start-route/start-route.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { MapBase } from '../../../components/map.base';
 import { MapConfigService } from '../../../services/map-config.service';
@@ -15,10 +16,28 @@ export class StartRouteComponent extends MapBase implements AfterViewInit, OnDes
 
   @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef;
   
-  constructor(protected override mapConfig: MapConfigService,
+  coordinates: string = '';
+  destinationLatitude: number = 0;
+  destinationLongitude: number = 0;
+
+  constructor(private route: ActivatedRoute,
+    protected override mapConfig: MapConfigService,
         protected override locationService: LocationService
   ) {
     super(mapConfig, locationService);
+  }
+
+  ngOnInit() {
+    this.coordinates = this.route.snapshot.paramMap.get('coordinates') || '';
+
+        // Split coordinates by comma
+    const coordArray = this.coordinates.split(',');
+    this.destinationLatitude = parseFloat(coordArray[0]) || 0;
+    this.destinationLongitude = parseFloat(coordArray[1]) || 0;
+
+    console.log(`[Lattude, Longitude]: [${this.destinationLatitude}, ${this.destinationLongitude}]`);
+
+    this.setDestinationCoordinates(this.destinationLatitude, this.destinationLongitude);
   }
 
   ngAfterViewInit(): void {
